cbaf1750242159cf9ce460dca6f24a9f801f8da3
MN-Cool <78140825+MN-Cool@users.noreply.github.com>
2021-03-26 19:54:45 +0000

    3rd Person Setup Pt. 7

--- Vision Game/Assets/Scripts/ThirdPersonController.cs
+++ Vision Game/Assets/Scripts/ThirdPersonController.cs
@@ -9,16 +9,24 @@ namespace UnityEngine.InputSystem
 
     public class ThirdPersonController : MonoBehaviour
     {
+        [SerializeField] private float speed = 10f;
+        [SerializeField] private float rotationSmoothing = 0.1f;
+
         private Vector3 movement;
+        private CharacterController myController;
+        private float rotateVelocity;
 
         void Start()
         {
-
+            myController = GetComponent<CharacterController>();
         }
 
         void Update()
         {
-
+            float angle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref rotateVelocity, rotationSmoothing);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            myController.Move(movement.normalized * (speed * Time.deltaTime));
         }
 
         public void Move(InputAction.CallbackContext context)
