847e2ea8d6bda0a706b1f999e9ab89d7b5c028d4
mncoolno.1 <c20334611@mytudublin.ie>
2021-05-01 20:07:15 +0100

    Adding more length and coins into level

---
+++ Vision Game/Assets/Scripts/CoinCollection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollection : MonoBehaviour
+{
+    [SerializeField] private GameManager manager;
+    [SerializeField] private int scoreToGive = 10;
+    public float rotationSpeed = 90;
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            manager.AddScore(scoreToGive);
+            Destroy(this.gameObject);
+        }
+    }
+}
